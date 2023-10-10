a80c82a6d364ffc7eca1ef44335688b672390408
chanu <cw8881@naver.com>
2023-10-10 09:34:40 +0900

    feat : SignUpActivity, xml 생성, manifest 등록

---
+++ app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
@@ -0,0 +1,10 @@
+package org.sopt.dosopttemplate
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
