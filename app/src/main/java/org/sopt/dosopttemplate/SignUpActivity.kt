586590a73befa7ed8c33f46f0b2c6e24118d3787
chanu <cw8881@naver.com>
2023-10-10 20:33:21 +0900

    feat : snackbar생성 및 각 조건별로 메세지 송출

--- app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
@@ -1,7 +1,9 @@
 package org.sopt.dosopttemplate
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import org.sopt.dosopttemplate.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
@@ -24,18 +26,37 @@ class SignUpActivity : AppCompatActivity() {
             val nickName = editNickname.text.toString()
             val mbti = editMbti.text.toString()
 
-//            if, else문 축약
             arrayFlag[0] = id.length in 6..10
-
             arrayFlag[1] = pwd.length in 8..12
-
             arrayFlag[2] = nickName.length in 1..8
-
             arrayFlag[3] = mbti.length == 4
 
-            /*    for (i in arrayFlag.indices) {
-                    Log.d("flag", i.toString() + arrayFlag[i].toString())
-                }*/
+            if (arrayFlag.all { it }) {
+                val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
+                startActivity(intent)
+            } else {
+                val snackList = mutableListOf<String>()
+                if (!arrayFlag[0]) {
+                    snackList.add("ID는 6자 이상 10자 이하여야 합니다.")
+                }
+                if (!arrayFlag[1]) {
+                    snackList.add("비밀번호는 8자 이상 12자 이하여야 합니다.")
+                }
+                if (!arrayFlag[2]) {
+                    snackList.add("닉네임은 1자 이상 8자 이하여야 합니다.")
+                }
+                if (!arrayFlag[3]) {
+                    snackList.add("MBTI는 4자여야 합니다.")
+                }
+
+                val snackMessage = snackList.joinToString("\n")
+
+                Snackbar.make(
+                    root,
+                    snackMessage,
+                    Snackbar.LENGTH_SHORT,
+                ).show()
+            }
         }
     }
 }
