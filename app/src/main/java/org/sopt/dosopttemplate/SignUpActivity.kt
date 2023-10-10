df3d97c29da32728898df6d6d60ca5eb309c98a1
chanu <cw8881@naver.com>
2023-10-10 19:13:59 +0900

    feat : 회원가입 조건 충족 array 확인

--- app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/SignUpActivity.kt
@@ -2,9 +2,40 @@ package org.sopt.dosopttemplate
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.sopt.dosopttemplate.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initSignUp()
+    }
+
+    private fun initSignUp() = with(binding) {
+        val arrayFlag = booleanArrayOf(false, false, false, false)
+
+        btnLogin.setOnClickListener {
+            val id = editId.text.toString()
+            val pwd = editPwd.text.toString()
+            val nickName = editNickname.text.toString()
+            val mbti = editMbti.text.toString()
+
+//            if, else문 축약
+            arrayFlag[0] = id.length in 6..10
+
+            arrayFlag[1] = pwd.length in 8..12
+
+            arrayFlag[2] = nickName.length in 1..8
+
+            arrayFlag[3] = mbti.length == 4
+
+            /*    for (i in arrayFlag.indices) {
+                    Log.d("flag", i.toString() + arrayFlag[i].toString())
+                }*/
+        }
     }
 }
