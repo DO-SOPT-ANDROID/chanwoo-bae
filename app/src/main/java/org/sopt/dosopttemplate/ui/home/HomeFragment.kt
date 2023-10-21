66dc5ebfe835b019c516e40d6a900162aea480b3
chanu <cw8881@naver.com>
2023-10-22 03:49:47 +0900

    [feat] : recyclerview 생성 및 더미데이터 추가 #4

--- app/src/main/java/org/sopt/dosopttemplate/ui/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/dosopttemplate/ui/home/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.data.home.Friend
 import org.sopt.dosopttemplate.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -12,6 +14,8 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding
         get() = requireNotNull(_binding) { "바인딩 객체 생성" }
+
+    private val mockFriendList = ArrayList<Friend>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,7 +29,22 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//         코드 부분
+//        가짜 데이터 추가
+        mockFriendList.add(Friend(R.drawable.gingerbread, "손흥민", "안녕하세요"))
+        mockFriendList.add(Friend(R.drawable.gingerbread, "손흥민", "안녕하세요"))
+        mockFriendList.add(Friend(R.drawable.gingerbread, "손흥민", "안녕하세요"))
+        mockFriendList.add(Friend(R.drawable.gingerbread, "손흥민", "안녕하세요"))
+        mockFriendList.add(Friend(R.drawable.gingerbread, "손흥민", "안녕하세요"))
+        mockFriendList.add(Friend(R.drawable.gingerbread, "손흥민", "안녕하세요"))
+        mockFriendList.add(Friend(R.drawable.gingerbread, "손흥민", "안녕하세요"))
+        mockFriendList.add(Friend(R.drawable.gingerbread, "손흥민", "안녕하세요"))
+        mockFriendList.add(Friend(R.drawable.gingerbread, "손흥민", "안녕하세요"))
+        mockFriendList.add(Friend(R.drawable.gingerbread, "손흥민", "안녕하세요"))
+        mockFriendList.add(Friend(R.drawable.gingerbread, "손흥민", "안녕하세요"))
+
+        val friendAdapter = FriendAdapter(requireContext())
+        binding.rvFriends.adapter = friendAdapter
+        friendAdapter.setFriendList(mockFriendList)
     }
 
     override fun onDestroyView() {
