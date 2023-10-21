360fc99cce0fbf840d6ff694e4434e45c02488bf
chanu <cw8881@naver.com>
2023-10-21 23:14:50 +0900

    [add] : MyPageFragment 생성 #4

---
+++ app/src/main/java/org/sopt/dosopttemplate/home/MyPageFragment.kt
@@ -0,0 +1,33 @@
+package org.sopt.dosopttemplate.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.sopt.dosopttemplate.databinding.FragmentMyPageBinding
+
+class MyPageFragment : Fragment() {
+
+    private var _binding: FragmentMyPageBinding? = null
+    private val binding: FragmentMyPageBinding
+        get() = requireNotNull(_binding) { "하하하" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
