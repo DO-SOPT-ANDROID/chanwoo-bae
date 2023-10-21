5313d600564f6149ffc1fd2be3f4470c5c9ee66b
chanu <cw8881@naver.com>
2023-10-22 03:28:23 +0900

    [add] : FriendAdapter 생성 #4

---
+++ app/src/main/java/org/sopt/dosopttemplate/ui/home/FriendAdapter.kt
@@ -0,0 +1,33 @@
+package org.sopt.dosopttemplate.ui.home
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import org.sopt.dosopttemplate.data.home.Friend
+import org.sopt.dosopttemplate.databinding.ItemFriendBinding
+
+class FriendAdapter(context: Context) : RecyclerView.Adapter<FriendViewHolder>() {
+    private val inflater by lazy { LayoutInflater.from(context) }
+
+    private var friendList: List<Friend> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FriendViewHolder {
+        val binding = ItemFriendBinding.inflate(inflater, parent, false)
+        return FriendViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FriendViewHolder, position: Int) {
+        holder.onBind(friendList[position])
+    }
+
+    override fun getItemCount() = friendList.size
+
+    // 리스트 연결
+    @SuppressLint("NotifyDataSetChanged")
+    fun setFriendList(friendList: List<Friend>) {
+        this.friendList = friendList.toList()
+        notifyDataSetChanged()
+    }
+}
