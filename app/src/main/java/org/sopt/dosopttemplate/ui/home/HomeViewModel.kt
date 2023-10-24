ac68370eabef746232fba55bdb64c4e6b23928b1
chanu <cw8881@naver.com>
2023-10-25 03:53:26 +0900

    #4 [add] : HomeViewModel 더미데이터 추가

--- app/src/main/java/org/sopt/dosopttemplate/ui/home/HomeViewModel.kt
+++ app/src/main/java/org/sopt/dosopttemplate/ui/home/HomeViewModel.kt
@@ -2,22 +2,65 @@ package org.sopt.dosopttemplate.ui.home
 
 import androidx.lifecycle.ViewModel
 import org.sopt.dosopttemplate.R
-import org.sopt.dosopttemplate.data.home.Friend
+import org.sopt.dosopttemplate.data.home.HomeSealedItem
 
 class HomeViewModel : ViewModel() {
 
-    val mockFriendList = listOf<Friend>(
-        Friend(
+    val mockProfileList = listOf<HomeSealedItem.MyProfile>(
+        HomeSealedItem.MyProfile(
+            myProfileImg = R.drawable.gingerbread,
+            myName = "배찬우",
+            my_description = "멀티리사이클러뷰 어렵네",
+        ),
+    )
+
+    val mockFriendList = listOf<HomeSealedItem.Friend>(
+        HomeSealedItem.Friend(
             profileImage = R.drawable.gingerbread,
             name = "전성기 시절 파트장",
             self_description = "꼼짝마 손들어",
         ),
-        Friend(
+        HomeSealedItem.Friend(
             profileImage = R.drawable.gingerbread,
             name = "손흥민",
             self_description = "나보다 잘생긴 사람 있으면 나와",
         ),
-        Friend(
+        HomeSealedItem.Friend(
+            profileImage = R.drawable.gingerbread,
+            name = "파트장",
+            self_description = "표정 풀자",
+        ),
+        HomeSealedItem.Friend(
+            profileImage = R.drawable.gingerbread,
+            name = "파트장",
+            self_description = "표정 풀자",
+        ),
+        HomeSealedItem.Friend(
+            profileImage = R.drawable.gingerbread,
+            name = "파트장",
+            self_description = "표정 풀자",
+        ),
+        HomeSealedItem.Friend(
+            profileImage = R.drawable.gingerbread,
+            name = "파트장",
+            self_description = "표정 풀자",
+        ),
+        HomeSealedItem.Friend(
+            profileImage = R.drawable.gingerbread,
+            name = "파트장",
+            self_description = "표정 풀자",
+        ),
+        HomeSealedItem.Friend(
+            profileImage = R.drawable.gingerbread,
+            name = "파트장",
+            self_description = "표정 풀자",
+        ),
+        HomeSealedItem.Friend(
+            profileImage = R.drawable.gingerbread,
+            name = "파트장",
+            self_description = "표정 풀자",
+        ),
+        HomeSealedItem.Friend(
             profileImage = R.drawable.gingerbread,
             name = "파트장",
             self_description = "표정 풀자",
