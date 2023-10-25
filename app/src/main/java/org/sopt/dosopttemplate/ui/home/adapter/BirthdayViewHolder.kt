e1ff826a562408bfddc36d6fda8a2db7e40e315d
chanu <cw8881@naver.com>
2023-10-26 04:40:07 +0900

    #4 [fix] : BirthdayViewHolder 선물하기, 멜론뮤직 2가지 경우로 구성

--- app/src/main/java/org/sopt/dosopttemplate/ui/home/adapter/BirthdayViewHolder.kt
+++ app/src/main/java/org/sopt/dosopttemplate/ui/home/adapter/BirthdayViewHolder.kt
@@ -1,9 +1,12 @@
 package org.sopt.dosopttemplate.ui.home.adapter
 
+import DateUtils
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import org.sopt.dosopttemplate.data.home.HomeSealedItem
 import org.sopt.dosopttemplate.databinding.ItemBirthdayBinding
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class BirthdayViewHolder(private val binding: ItemBirthdayBinding) :
     RecyclerView.ViewHolder(binding.root) {
@@ -11,32 +14,37 @@ class BirthdayViewHolder(private val binding: ItemBirthdayBinding) :
     fun onBind(birthData: HomeSealedItem.Birthday) = with(binding) {
         imgProfile.setImageResource(birthData.profileImage)
         tvName.text = birthData.name
-        tvBirthDate.text = DateUtils.formatCustomDate(birthData.date)
 
-        // 멜론 뮤직 없을 경우에 cardview또한 안보이도록
-        if (birthData.self_description.isEmpty()) {
-            cvGreen.visibility = View.INVISIBLE
-        } else {
-            tvSelfDescription.text = birthData.self_description
-            cvGreen.visibility = View.VISIBLE
+        val date = LocalDate.parse(birthData.date)
+        val formatter = DateTimeFormatter.ofPattern("M월 d일")
+
+        when (DateUtils.getDateOrder(birthData.date)) {
+            0 -> {
+                cvRedVisibility()
+                tvBirthDate.text = "오늘 ${date.format(formatter)}"
+            }
+
+            1 -> {
+                cvRedVisibility()
+                tvBirthDate.text = "어제 ${date.format(formatter)}"
+            }
+
+            else -> {
+                tvBirthDate.visibility = View.GONE
+                cvRed.visibility = View.GONE
+                // 멜론 뮤직 없을 경우에 cardview또한 안보이도록
+                if (birthData.self_description.isEmpty()) {
+                    cvGreen.visibility = View.GONE
+                } else {
+                    tvSelfDescription.text = birthData.self_description
+                    cvGreen.visibility = View.VISIBLE
+                }
+            }
         }
     }
 
-    /* private fun formatCustomDate(inputDate: String): String {
-         // 입력된 날짜를 LocalDate로 파싱
-         val date = LocalDate.parse(inputDate)
-
-         val today = LocalDate.now()
-         val yesterday = today.minusDays(1)
-
-         // 원하는 날짜 형식을 정의 (예: "M월 d일")
-         val formatter = DateTimeFormatter.ofPattern("M월 d일")
-
-         // LocalDate를 지정된 형식으로 포맷팅
-         return when (date) {
-             today -> "오늘 ${date.format(formatter)}"
-             yesterday -> "어제 ${date.format(formatter)}"
-             else -> date.format(formatter)
-         }
-     }*/
+    private fun ItemBirthdayBinding.cvRedVisibility() {
+        cvGreen.visibility = View.GONE
+        cvRed.visibility = View.VISIBLE
+    }
 }
