aaaa10f489cac0153bb08dd7d9ac38b217a1ca56
chanu <cw8881@naver.com>
2023-11-10 18:01:23 +0900

    [chore] : 주석추가

--- app/src/main/java/org/sopt/dosopttemplate/ui/home/adapter/HomeDiffCallBack.kt
+++ app/src/main/java/org/sopt/dosopttemplate/ui/home/adapter/HomeDiffCallBack.kt
@@ -4,6 +4,8 @@ import androidx.recyclerview.widget.DiffUtil
 import org.sopt.dosopttemplate.data.home.HomeSealedItem
 
 object HomeDiffCallBack : DiffUtil.ItemCallback<HomeSealedItem>() {
+
+    //    이거 왜 javaClass로 비교해야하는지 아시는 분 리뷰 달아주세요..!
     override fun areItemsTheSame(oldItem: HomeSealedItem, newItem: HomeSealedItem): Boolean {
         return oldItem.javaClass == newItem.javaClass
     }
