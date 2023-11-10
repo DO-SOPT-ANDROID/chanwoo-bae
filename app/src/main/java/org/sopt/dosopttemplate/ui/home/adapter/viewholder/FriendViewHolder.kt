a5a256694b53ead6144caf30dd0f60780712c534
chanu <cw8881@naver.com>
2023-11-10 16:57:00 +0900

    [chore] : 디렉토리 구조 변경

--- app/src/main/java/org/sopt/dosopttemplate/ui/home/adapter/viewholder/FriendViewHolder.kt
+++ app/src/main/java/org/sopt/dosopttemplate/ui/home/adapter/viewholder/FriendViewHolder.kt
@@ -1,4 +1,4 @@
-package org.sopt.dosopttemplate.ui.home.adapter
+package org.sopt.dosopttemplate.ui.home.adapter.viewholder
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
