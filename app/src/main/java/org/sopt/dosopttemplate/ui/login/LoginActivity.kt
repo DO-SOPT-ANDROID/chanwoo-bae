1e0faf2f447118779a2ae29cb4c27ec1a7ad66cc
chanu <cw8881@naver.com>
2023-11-17 19:56:55 +0900

    [feat] : 로그인 서버 연결 및 request

--- app/src/main/java/org/sopt/dosopttemplate/ui/login/LoginActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/ui/login/LoginActivity.kt
@@ -3,14 +3,20 @@ package org.sopt.dosopttemplate.ui.login
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import org.sopt.dosopttemplate.data.user.UserInfo
 import org.sopt.dosopttemplate.databinding.ActivityLoginBinding
+import org.sopt.dosopttemplate.network.ApiFactory.ServicePool.authService
+import org.sopt.dosopttemplate.network.login.RequestLoginDto
+import org.sopt.dosopttemplate.network.login.ResponseLoginDto
 import org.sopt.dosopttemplate.ui.HomeActivity
-import java.util.ArrayList
+import retrofit2.Call
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
 
@@ -34,15 +40,42 @@ class LoginActivity : AppCompatActivity() {
         btnLogin.setOnClickListener {
             val id = editId.text.toString()
             val pwd = editPwd.text.toString()
-            if (receivedList.isEmpty()) {
-                showSnackMessage("회원가입을 먼저 진행해 주세요")
-            } else if (id == receivedList[0] && pwd == receivedList[1]) {
-                sendUserData(receivedList)
-                finish()
-            } else {
-                showSnackMessage("ID와 password가 일치하지 않습니다")
-            }
+
+            authLogin(id, pwd)
         }
+    }
+
+    private fun authLogin(id: String, pwd: String) {
+        authService.login(RequestLoginDto(id, pwd))
+            .enqueue(
+                object : retrofit2.Callback<ResponseLoginDto> {
+                    override fun onResponse(
+                        call: Call<ResponseLoginDto>,
+                        response: Response<ResponseLoginDto>,
+                    ) {
+                        if (response.isSuccessful) {
+                            val data = response.body()!!
+                            val userId = data.id
+                            Toast.makeText(
+                                this@LoginActivity,
+                                "로그인이 성공하였고 유저의 ID는 $userId 입니둥",
+                                Toast.LENGTH_SHORT,
+                            ).show()
+                            val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+                            startActivity(intent)
+                        } else {
+                            showSnackMessage("아이디와 패스워드가 일치 하지 않습니다.")
+                            Log.e("LoginActivity", "Error: ${response.message()}")
+                        }
+                    }
+
+                    override fun onFailure(call: Call<ResponseLoginDto>, t: Throwable) {
+                        Toast.makeText(this@LoginActivity, "서버 에러 발생", Toast.LENGTH_SHORT).show()
+                        Log.e("LoginActivity", "Error: ${t.message}")
+                    }
+                },
+
+            )
     }
 
     private fun sendUserData(receivedList: ArrayList<String>) {
