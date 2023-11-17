d39fc989d50508bf55d518674522975cf0b383b5
chanu <cw8881@naver.com>
2023-11-17 19:59:51 +0900

    [feat] : 회원가입 서버통신 및 관련 dto 작성

--- app/src/main/java/org/sopt/dosopttemplate/ui/login/SignUpActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/ui/login/SignUpActivity.kt
@@ -3,11 +3,16 @@ package org.sopt.dosopttemplate.ui.login
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import org.sopt.dosopttemplate.databinding.ActivitySignUpBinding
+import org.sopt.dosopttemplate.network.ApiFactory.ServicePool.authService
+import org.sopt.dosopttemplate.network.login.RequestSignUpDto
+import retrofit2.Call
+import retrofit2.Response
 
 class SignUpActivity : AppCompatActivity() {
 
@@ -44,14 +49,14 @@ class SignUpActivity : AppCompatActivity() {
                 editMbti.text.toString(),
             )
             // if else문 축약
-            arrayFlag[0] = userInputList[0].length in 6..10
+            arrayFlag[0] = userInputList[0].length in 4..10
             arrayFlag[1] = userInputList[1].length in 8..12
             arrayFlag[2] = userInputList[2].length in 1..8
             arrayFlag[3] = userInputList[3].length == 4
 
             // 모든 조건이 true인 경우 로그인 화면으로 이동
             if (arrayFlag.all { it }) {
-                sendUserData(userInputList)
+                signUpServer(userInputList)
             } else {
                 // 추후에 textwathcer로 변경
                 showSnackMessage(arrayFlag)
@@ -59,12 +64,31 @@ class SignUpActivity : AppCompatActivity() {
         }
     }
 
+    private fun signUpServer(userInputList: List<String>) {
+        authService.signUp(RequestSignUpDto(userInputList[0], userInputList[1], userInputList[2]))
+            .enqueue(object : retrofit2.Callback<Unit> {
+                override fun onResponse(call: Call<Unit>, response: Response<Unit>) {
+                    if (response.isSuccessful) {
+                        sendUserData(userInputList)
+                        Toast.makeText(this@SignUpActivity, "회원가입이 완료되었습니다", Toast.LENGTH_SHORT)
+                            .show()
+                    } else {
+                        Toast.makeText(this@SignUpActivity, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+                override fun onFailure(call: Call<Unit>, t: Throwable) {
+                    Toast.makeText(this@SignUpActivity, "네트워크 연결 오류", Toast.LENGTH_SHORT).show()
+                    Log.e("SignUpActivity", "Error: ${t.message}")
+                }
+            })
+    }
+
     private fun sendUserData(userInputList: List<String>) {
         val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
         intent.putStringArrayListExtra("userInputList", ArrayList(userInputList))
         // LoginActivity로 결과를 반환
         setResult(RESULT_OK, intent)
-        Toast.makeText(this, "회원가입이 완료되었습니다", Toast.LENGTH_SHORT).show()
         // 결과 반환 후 현재 액티비티 종료
         finish()
     }
@@ -72,7 +96,7 @@ class SignUpActivity : AppCompatActivity() {
     private fun showSnackMessage(arrayFlag: BooleanArray) {
         val snackList = mutableListOf<String>()
         if (!arrayFlag[0]) {
-            snackList.add("ID는 6자 이상 10자 이하여야 합니다.")
+            snackList.add("ID는 4자 이상 10자 이하여야 합니다.")
         }
         if (!arrayFlag[1]) {
             snackList.add("비밀번호는 8자 이상 12자 이하여야 합니다.")
