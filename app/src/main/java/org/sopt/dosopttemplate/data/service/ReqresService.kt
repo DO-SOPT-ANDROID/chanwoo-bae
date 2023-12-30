4941b93861a9194ee674dd21b422723497c6ab4e
chanu <cw8881@naver.com>
2023-12-31 02:20:54 +0900

    [fix] : ReqresService suspend 함수 변경, return type 변경

--- app/src/main/java/org/sopt/dosopttemplate/data/service/ReqresService.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/service/ReqresService.kt
@@ -1,13 +1,12 @@
 package org.sopt.dosopttemplate.data.service
 
 import org.sopt.dosopttemplate.data.dto.remote.respose.ResponseReqresDto
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface ReqresService {
     @GET("api/users")
-    fun getUserList(
+    suspend fun getUserList(
         @Query("page") page: Int,
-    ): Call<ResponseReqresDto>
+    ): ResponseReqresDto
 }
