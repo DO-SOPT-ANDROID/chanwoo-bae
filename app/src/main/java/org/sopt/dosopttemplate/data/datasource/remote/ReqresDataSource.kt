b6b52122592a9b6f15b9ff430652f37ef2438997
chanu <cw8881@naver.com>
2024-01-03 06:46:14 +0900

    [chore] : 주석 추가

--- app/src/main/java/org/sopt/dosopttemplate/data/datasource/remote/ReqresDataSource.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/datasource/remote/ReqresDataSource.kt
@@ -2,6 +2,7 @@ package org.sopt.dosopttemplate.data.datasource.remote
 
 import org.sopt.dosopttemplate.data.dto.remote.respose.ResponseReqresDto
 
+// 레포지터리 인터페이스, 데이터 접근을 추상화, 데이터를 요청하는 코드를 부름
 interface ReqresDataSource {
     suspend fun getReqresList(page: Int): ResponseReqresDto
 }
--- app/src/main/java/org/sopt/dosopttemplate/data/datasourceimpl/remote/ReqresDataSourceImpl.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/datasourceimpl/remote/ReqresDataSourceImpl.kt
@@ -4,6 +4,7 @@ import org.sopt.dosopttemplate.data.datasource.remote.ReqresDataSource
 import org.sopt.dosopttemplate.data.dto.remote.respose.ResponseReqresDto
 import org.sopt.dosopttemplate.data.service.ReqresService
 
+// 실제 데이터 소스를 구현하는 클래스, 네트워크 호출을 통해 데이터를 가져오는 역할
 class ReqresDataSourceImpl(
     private val reqresService: ReqresService,
 ) : ReqresDataSource {
