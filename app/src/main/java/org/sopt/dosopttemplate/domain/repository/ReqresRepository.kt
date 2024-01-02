494c87f442f3f2515f5ca263ba3b3e5b62479608
chanu <cw8881@naver.com>
2024-01-03 04:57:14 +0900

    [feat] : domain 영역 ReqresRepository 추가(현재는 의미 없음)

--- app/src/main/java/org/sopt/dosopttemplate/domain/repository/ReqresRepository.kt
+++ app/src/main/java/org/sopt/dosopttemplate/domain/repository/ReqresRepository.kt
@@ -2,6 +2,8 @@ package org.sopt.dosopttemplate.domain.repository
 
 import org.sopt.dosopttemplate.domain.entity.ReqresEntity
 
+/*ReqresRepository 인터페이스는 도메인(Domain) 레이어에서 사용되며,
+원격 데이터 소스(ReqresDataSource)로부터 Reqres 사용자 리스트를 가져오는 기능을 정의*/
 interface ReqresRepository {
     suspend fun getReqresList(page: Int): Result<List<ReqresEntity>>
 }
