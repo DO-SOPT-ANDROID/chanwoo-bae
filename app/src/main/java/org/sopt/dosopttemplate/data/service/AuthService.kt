bfc726e4db9334eb8b70137aa68c16337d6c4a3c
chanu <cw8881@naver.com>
2024-01-03 17:54:18 +0900

    [chore] : AuthService 콜백 타입 수정

--- app/src/main/java/org/sopt/dosopttemplate/data/service/AuthService.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/service/AuthService.kt
@@ -3,7 +3,6 @@ package org.sopt.dosopttemplate.data.service
 import org.sopt.dosopttemplate.data.dto.remote.request.RequestLoginDto
 import org.sopt.dosopttemplate.data.dto.remote.request.RequestSignUpDto
 import org.sopt.dosopttemplate.data.dto.remote.respose.ResponseLoginDto
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
@@ -11,10 +10,10 @@ interface AuthService {
     @POST("api/v1/members/sign-in")
     suspend fun postLogin(
         @Body request: RequestLoginDto,
-    ): Response<ResponseLoginDto>
+    ): ResponseLoginDto
 
     @POST("api/v1/members")
     suspend fun postSignUp(
         @Body request: RequestSignUpDto,
-    ): Response<Unit>
+    ): Unit
 }
