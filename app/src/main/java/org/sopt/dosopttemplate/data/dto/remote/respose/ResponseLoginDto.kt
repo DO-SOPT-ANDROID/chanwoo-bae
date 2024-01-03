f84ee04aaed1208912ffb768c02c89364265de4f
chanu <cw8881@naver.com>
2024-01-03 17:59:31 +0900

    [feat] : ResponseLoginDto map 함수 추가

--- app/src/main/java/org/sopt/dosopttemplate/data/dto/remote/respose/ResponseLoginDto.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/dto/remote/respose/ResponseLoginDto.kt
@@ -2,6 +2,7 @@ package org.sopt.dosopttemplate.data.dto.remote.respose
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.sopt.dosopttemplate.domain.entity.UserEntity
 
 @Serializable
 data class ResponseLoginDto(
@@ -10,5 +11,12 @@ data class ResponseLoginDto(
     @SerialName("username")
     val username: String,
     @SerialName("nickname")
-    val nickname: String? = null,
-)
+    val nickname: String,
+) {
+    fun toUserEntity() = UserEntity(
+        id = username,
+        pwd = "",
+        nickName = nickname,
+        mbti = "",
+    )
+}
